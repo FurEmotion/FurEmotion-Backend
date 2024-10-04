92eec0f487abb7ad089a54357d1eba04f22bfd1c
jaewonE <importjaewonE@gmail.com>
2024-10-04 20:35:29 +0900

    Refactor log.py to create 'logs' directory if it doesn't exist

--- log.py
+++ log.py
@@ -2,6 +2,9 @@ import logging
 import logging.config
 import os
 
+if not os.path.exists('logs'):
+    os.makedirs('logs', exist_ok=True)
+
 # 로깅 설정 파일 경로
 logging_config_path = os.path.join(os.path.dirname(__file__), 'logging.conf')
 logging.config.fileConfig(logging_config_path, disable_existing_loggers=False)
